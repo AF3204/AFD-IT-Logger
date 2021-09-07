3a6f5e0d68cb7f244212050e857f312859012376
Faizal Dar-Sandur <afaizal1987@gmail.com>
2021-09-07 15:16:00 +0800

    UPDATED: Materialize Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * 20210907 - useEffect is used to initialise the materialize CSS
+ *          - equivalent to eventListener in the HTML
+ *   */ 
+import React, {Fragment,useEffect} from "react";
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-function App() {
+
+const App = () => {
+
+  // Using useEffect to initalise the css framework
+  useEffect(()=>{
+    M.AutoInit();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      IT Logger App
     </div>
   );
 }
